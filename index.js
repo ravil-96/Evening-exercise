0f584d2c5db6755dc2dbf022d5eadce707dbc9e5
Ravil <ravil-96@mail.ru>
2021-03-08 16:10:18 +0000

    adding tests to our flow

---
+++ index.js
@@ -0,0 +1,13 @@
+function sayHi(){
+    console.log("Hi");
+}
+
+function add(a, b){
+    return a + b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+module.exports = { add, multiply }
---
+++ tests/spec.js
@@ -0,0 +1,8 @@
+const myFuncs = require('../index.js')
+
+describe ('addThese', ()=>{
+   test ('addThese', ()=>{
+      expect(myFuncs.add).toBeDefined();
+   })
+})
+
